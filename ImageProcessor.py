8f0cf86bc3cc780c6f07609bc3d7731e2072a0d0
Sienki <zonced@gmail.com>
2019-04-16 16:00:21 +0200

    Code refactor, split code into classes.

---
+++ ImageProcessor.py
@@ -0,0 +1,147 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import skimage.io as io
+import skimage.data as data
+import skimage.color as color
+import skimage.measure
+import skimage
+import cv2
+
+
+class ImageProcessor:
+    def __init__(self):
+        self.img = None
+        self.result = None
+        self.name = ""
+
+    def get_sub_image(self, rect, src):
+        center, size, theta = rect
+        center, size = tuple(map(int, center)), tuple(map(int, size))
+        (h, w) = src.shape[:2]
+        (cX, cY) = center
+        M, nW, nH = self.get_rotation_matrix(cX, cY, theta, h, w)
+        dst = cv2.warpAffine(src, M, (nW, nH))
+        out = cv2.getRectSubPix(dst, size, (nW/2, nH/2))
+        return out
+
+    def get_final_rotation(self, image, base):
+        angle = 0
+        (h, w) = image.shape[:2]
+        (cX, cY) = (w//2, h//2)
+        if base[0][0] == 0 and base[1][1] == 0:
+            angle = 90
+        elif base[0][0] == 0 and base[1][0] == 0:
+            angle = 180
+        elif base[0][1] != 0:
+            angle = -90
+        M, nW, nH = self.get_rotation_matrix(cX, cY, angle, h, w)
+        out = cv2.warpAffine(image, M, (nW, nH))
+        return out
+
+    @staticmethod
+    def get_four_parts_indices(img_shape):
+        # It returns sets of ranges (each for one corner)
+        rows, cols = img_shape
+        rows_cond = rows % 2
+        cols_cond = cols % 2
+        results = []
+        center_x = int(cols / 2)
+        center_y = int(rows / 2)
+        if rows_cond and cols_cond:  #
+            center_y = int(rows / 2) + 1
+            center_x = int(cols / 2) + 1
+        elif rows_cond:  # only y is odd
+            center_y = int(rows / 2) + 1
+        elif cols_cond:  # only x is odd
+            center_x = int(cols / 2) + 1
+        results.append(((0, 0), (center_y, center_x)))
+        results.append(((center_y, 0), (rows, center_x)))
+        results.append(((center_y, center_x), (rows, cols)))
+        results.append(((0, center_x), (center_y, cols)))
+        return results
+
+    @staticmethod
+    def find_base(img_shape, areas):
+        # It finds a base of the rectangle, chooses maximum sum from two adjacent rectangles
+        rows, cols = img_shape
+        areas_sum = []
+        for i, _ in enumerate(areas):
+            if i != len(areas) - 1:
+                areas_sum.append(areas[i] + areas[i + 1])
+            else:
+                areas_sum.append(areas[0] + areas[i])
+        arg_max_sum = np.argmax(areas_sum)
+        if arg_max_sum == 0:
+            return (0, 0), (rows, 0)
+        if arg_max_sum == 1:
+            return (rows, 0), (rows, cols)
+        if arg_max_sum == 2:
+            return (0, cols), (rows, cols)
+        else:
+            return (0, 0), (0, cols)
+
+    @staticmethod
+    def find_best_rectangle(contours):
+        # It finds the best rectangle from contour If there is more than one contour, it chooses the biggest
+        best_rect = None
+        off_1, off_2 = -1, -1
+        for contour in contours:
+            rect = cv2.minAreaRect(contour)
+            if rect[1][0] > off_1 and rect[1][1] > off_2:
+                best_rect = rect
+                off_1 = rect[1][0]
+                off_2 = rect[1][1]
+        return best_rect
+
+    @staticmethod
+    def get_rotation_matrix(cX, cY, angle, h, w):
+        M = cv2.getRotationMatrix2D((cX, cY), angle, 1.0)
+        cos = np.abs(M[0, 0])
+        sin = np.abs(M[0, 1])
+
+        nW = int((h * sin) + (w * cos))
+        nH = int((h * cos) + (w * sin))
+
+        M[0, 2] += (nW / 2) - cX
+        M[1, 2] += (nH / 2) - cY
+        return M, nW, nH
+
+    def process_image(self):
+        ret, thresh = cv2.threshold(self.img, 127, 255, 0)
+        _, contours, hierarchy = cv2.findContours(thresh, 1, 2)
+        if len(contours) > 1:
+            rect = self.find_best_rectangle(contours)
+        else:
+            rect = cv2.minAreaRect(contours[0])
+        self.result = self.get_sub_image(rect, thresh)
+
+        plt.show()
+        ranges = self.get_four_parts_indices(self.result.shape[:2])
+        areas = []
+
+        for range in ranges:
+            lower = range[0]
+            upper = range[1]
+            rectangle_part = self.result[lower[0]:upper[0], lower[1]: upper[1]]
+            arguments = np.argwhere(rectangle_part > 127)
+            areas.append(arguments.shape[0])
+
+        base = self.find_base(self.result.shape[:2], areas)
+        self.result = self.get_final_rotation(self.result, base)
+
+    def read_img(self, path):
+        self.img = io.imread(path)
+        self.name = path.split("\\")[-1]
+
+    def plot(self):
+        plt.figure(1)
+        plt.subplot(121)
+        plt.imshow(self.img)
+
+        plt.subplot(122)
+        plt.imshow(self.result)
+        plt.title(self.name)
+        plt.show()
+
+    def scale_image(self):
+
--- main.py
+++ main.py
@@ -1,145 +1,40 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import skimage.io as io
-import skimage.data as data
-import skimage.color as color
-import skimage
+from ImageProcessor import ImageProcessor
+from FFTMethod import FFTMethod
+
 import os
-import cv2
-from scipy.spatial import distance
-
-def _approx_equal(x, y, tolerance=0.01):
-    return abs(x-y) <= 0.5 * tolerance * (x + y)
 
 
-def _get_angle_declination(p1, p2):
-    x1, y1 = p1
-    x2, y2 = p2
-    dx = x2 - x1
-    dy = y2 - y1
-    rads = np.math.atan2(-dy, dx)
-    return np.math.degrees(rads)
+def main(set_name):
+    test_sets = {
+        "set0": os.path.join(".", "proj1_daneA", "set0"),
+        "set1": os.path.join(".", "proj1_daneA", "set1"),
+        "set2": os.path.join(".", "proj1_daneA", "set2"),
+        "set3": os.path.join(".", "proj1_daneA", "set3"),
+        "set4": os.path.join(".", "proj1_daneA", "set4"),
+        "set5": os.path.join(".", "proj1_daneA", "set5"),
+        "set6": os.path.join(".", "proj1_daneA", "set6"),
+        "set7": os.path.join(".", "proj1_daneB", "set7"),
+        "set8": os.path.join(".", "proj1_daneB", "set8"),
+    }
+    set_dir = test_sets[set_name]
 
-
-def _check_angles(declinations):
-    angles = []
-    print(declinations)
-    base_found = False
-    if len(declinations) not in {0, 1}:
-        for i in range(0, len(declinations)):
-            for j in range(i+1, len(declinations)):
-                angles.append((abs(declinations[i] - declinations[j]), i, j))
-    elif len(declinations) == 1:
-        angles.append(0)
-
-    probable_indices_of_base = []
-    print(angles)
-    print(len(angles))
-    if len(angles) == 1:
-        print("Two lines found")
-        if _approx_equal(angles[0][0], 90, 0.05):
-            print('Found a probable base')
-            probable_indices_of_base.append(angles[0][1])
-            probable_indices_of_base.append(angles[0][2])
-    else:
-        for i in range(0, len(angles)):
-            for j in range(i+1, len(angles)):
-                    if angles[i][1] == angles[j][1] or angles[i][2] == angles[j][2]:
-                        if _approx_equal(angles[i][0] + angles[j][0], 180, 0.05):
-                            print('Found a probable base')
-                            print(angles[i][0] + angles[j][0])
-                            probable_indices_of_base.append(list(set.intersection(set(angles[i]), set(angles[j])))[0])
-
-    print(probable_indices_of_base)
-    if len(probable_indices_of_base) is not 0:
-        base_found = True
-    else:
-        probable_indices_of_base.append(0)
-    print(angles)
-    return base_found, probable_indices_of_base
-
-
-def _hough_transform(edges, img):
-    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 100, 5, 20, 20)  # czwarty parametr to minimalna długość linii
-    declinations = []
-    if lines is not None:
-        if len(lines) > 1:
-            for i in range(0, len(lines)):
-                # Standardowa tranformacja Hougha cv2.HoughLines(...)
-                # rho = lines[i][0][0]
-                # theta = lines[i][0][1]
-                # a = np.math.cos(theta)
-                # b = np.math.sin(theta)
-                # x0 = a * rho
-                # y0 = b * rho
-                # pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * a))
-                # pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * a))
-                # cv2.line(edges, pt1, pt2, (110, 40, 255), 3, cv2.LINE_AA)
-
-                # Probabilistyczna transformacja Hougha cv2.HoughLinesP(...)
-                line = lines[i][0]
-                declinations.append(_get_angle_declination((line[0], line[1]), (line[2], line[3])))
-                cv2.line(img, (line[0], line[1]), (line[2], line[3]), (110, 40, 255), 3, cv2.LINE_AA)
-        else:
-            for i in range(0, len(lines)):
-                line = lines[i][0]
-                cv2.line(img, (line[0], line[1]), (line[2], line[3]), (110, 40, 255), 3, cv2.LINE_AA)
-    # print(lines)
-    return declinations, lines
-
-
-def _process_image(img):
-    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    edges = cv2.Canny(img, 100, 200)
-    declinations, lines = _hough_transform(edges, img)
-    print(lines)
-    if lines is not None:
-        if len(lines) > 1:
-            found, index = _check_angles(declinations)
-            if found:
-                if len(index) == 1:
-                    line = lines[index][0][0]
-                    cv2.line(edges, (line[0], line[1]), (line[2], line[3]), (110, 40, 255), 3, cv2.LINE_AA)
-                if len(index) > 1:
-                    print("Several possible bases")
-                    line_lengths = []
-                    for i in index:
-                        line = lines[i][0]
-                        p1 = (int(line[0]), int(line[1]))
-                        p2 = (int(line[2]), int(line[3]))
-                        print(p1)
-                        dst = distance.euclidean(p1, p2)
-                        line_lengths.append(dst)
-                        print(line_lengths)
-                    max_line_index = line_lengths.index(max(line_lengths))
-                    line = lines[max_line_index][0]
-                    cv2.line(edges, (line[0], line[1]), (line[2], line[3]), (110, 40, 255), 3, cv2.LINE_AA)
-        else:
-            print("One line found")
-            line = lines[0][0]
-            cv2.line(edges, (line[0], line[1]), (line[2], line[3]), (110, 40, 255), 3, cv2.LINE_AA)
-    else:
-        print("Lines not found")
-    plt.subplot(121), plt.imshow(img, cmap='gray')
-    plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122), plt.imshow(edges, cmap='gray')
-    plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-    plt.show()
-
-
-def main():
-    datasetdir = os.path.join(".", "proj1_daneB", "set7")
-    for image in os.listdir(datasetdir):
+    for image in os.listdir(set_dir):
         if image.lower().endswith('png'):
-            path = os.path.join(datasetdir, image)
-            print(path)
-            img = io.imread(path)
-            _process_image(img)
-    # filedir = ".\\proj1_daneA\\set0\\1.png"
-    # img = io.imread(filedir)
-    # _process_image(img)
+            path = os.path.join(set_dir, image)
+            ip = ImageProcessor()
+            ip.read_img(path)
+            ip.process_image()
+            ip.plot()
+
+
+def test():
+    path = os.path.join(".", "proj1_daneA", "set2", "7.png")
+    ip = ImageProcessor()
+    ip.read_img(path)
+    ip.process_image()
+    ip.plot()
 
 
 if __name__ == "__main__":
-    main()
-
+    main("set0")
+    # test()
