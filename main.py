2de381820d5a9cab6c5cdbbb30b8c7ee9fb27f69
Sienki <zonced@gmail.com>
2019-04-17 22:00:25 +0200

    Final (?) fft version, acc: 75% - small amendment

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def check_ranks(ranks, path, N):
     return result
 
 
-def process(set_dir, image_count, plot=False, cc=1):
+def process(set_dir, image_count, plot=False, write_spec=False, cc=1):
     FFTMethod.clear_coefficients()
     fft_method = None
     for i in range(image_count):
@@ -43,8 +43,10 @@ def process(set_dir, image_count, plot=False, cc=1):
         fft_method.find_coefficients(ip.result)
 
     if fft_method:
-        ranks = fft_method.generate_rankings(print_out=True)
+        ranks = fft_method.generate_rankings(print_out=plot)
         results = check_ranks(ranks, set_dir, image_count)
+        if write_spec:
+            print(results)
         if plot:
             fft_method.plot_all_coefficients(only_print=True)
         return results[-1]
@@ -65,7 +67,7 @@ def from_bash(dir, N):
         fft_method.generate_rankings()
 
 
-def main(set_name, cc, plot, method_fft_coeffs=None, extend=None, extended_length=None):
+def main(set_name, cc, plot, write_spec=False):
     test_sets = {
         "set0": (os.path.join(".", "data", "set0"), 6),
         "set1": (os.path.join(".", "data", "set1"), 20),
@@ -77,7 +79,7 @@ def main(set_name, cc, plot, method_fft_coeffs=None, extend=None, extended_lengt
         "set7": (os.path.join(".", "data", "set7"), 20),
         "set8": (os.path.join(".", "data", "set8"), 100),
     }
-    return process(test_sets[set_name][0], test_sets[set_name][1], cc=cc)
+    return process(test_sets[set_name][0], test_sets[set_name][1], cc=cc, plot=plot, write_spec=write_spec)
     # from_bash(test_sets[set_name][0], test_sets[set_name][1])
 
 
@@ -98,8 +100,13 @@ def test(data_set, numbers, cc):
 
 
 if __name__ == "__main__":
-    res = main("set2", 5, True)
-    print(res)
+    all_sets_results = 0
+    for i in range(9):
+        s = "set{}".format(i)
+        res = main(s, 1, False, write_spec=True)
+        print(s, res)
+        all_sets_results += res
+    print("All sets res {} is {}%".format(all_sets_results, (all_sets_results / 606) * 100))
     # print("max", max_result, "params", params)
     # test("set1",[2, 5, 15], 20)
     # if len(sys.argv) != 3:
