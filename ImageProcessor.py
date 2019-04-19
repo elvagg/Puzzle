e952e2849cadeeca9ab1c360f32a5bd2b4064b7a
elvagg <paulinawarkocka@gmail.com>
2019-04-19 19:24:27 +0200

    Fixed perspective, 92.34%

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -155,11 +155,9 @@ class ImageProcessor:
 
         def det(a, b):
             return a[0] * b[1] - a[1] * b[0]
-
         div = det(xdiff, ydiff)
         if div == 0:
             raise Exception('lines do not intersect')
-
         d = (det(*line1), det(*line2))
         x = det(d, xdiff) / div
         y = det(d, ydiff) / div
